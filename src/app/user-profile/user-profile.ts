81d70fefb9de88e38a16c7759d326b37d9b4994a
LuckyJesus150 <pavlovov150@gmail.com>
2025-12-23 17:46:27 +0000

    fix: update header text

--- src/app/user-profile/user-profile.ts
+++ src/app/user-profile/user-profile.ts
@@ -23,13 +23,12 @@ export interface User {
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule], // Імпортуємо CommonModule для *ngFor
+  imports: [CommonModule],
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css'
 })
-export class UserProfile { // Назва класу коротка
+export class UserProfile {
   
-  // Створюємо об'єкт із даними
   user: User = {
     id: 101,
     name: 'Олександр Програміст',
@@ -41,7 +40,7 @@ export class UserProfile { // Назва класу коротка
     hobbies: ['Angular', 'TypeScript', 'Велоспорт', 'Фотографія']
   };
 
-  // Метод для підрахунку хобі
+
   getHobbiesCount(): number {
     return this.user.hobbies.length;
   }
